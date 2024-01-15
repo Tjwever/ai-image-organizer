0a297e3ddc4e019cc4b08d6bf70ee36fc4da8fc8
Tjwever <timjwever@gmail.com>
2024-01-15 13:09:23 -0500

    fixed the venv files so they don't contain anything they shouldnt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/timdog/main/python/venv"
+set -gx VIRTUAL_ENV "/home/wever/main/projects/ai-image-organizer/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
